23db57f041b15917c823515bbab0262b77c6bf17
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 13:17:28 +0100

    Create Continentes.java

---
+++ Continentes.java
@@ -0,0 +1,40 @@
+public class Continentes{
+
+  public static void main(String[] args){
+
+    int continente = 4;
+    
+    switch (continente) {
+    case 1:
+    System.out.println("Norte America, Mexico");
+    break;
+
+    case 2:
+    System.out.println("Sur America, Brasil");
+    break;
+
+    case 3:
+    System.out.println("Europa, Rusia");
+    break;
+
+    case 4:
+    System.out.println("Africa, Nigeria");
+    break;
+
+    case 5:
+    System.out.println("Asia, China");
+    break;
+
+    case 6:
+    System.out.println("Australia, Sydney");
+    break;
+
+    case 7:
+    System.out.println("Antartida");
+    break;
+
+    default:
+    System.out.println("No hay continente");
+    }
+  }
+}
